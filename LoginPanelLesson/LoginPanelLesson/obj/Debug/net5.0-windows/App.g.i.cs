0477d544b9532f173ccb09d0b74d5957867d3405
Killer31g <szymon192016@outlook.com>
2022-12-17 19:52:37 +0100

    edit

--- LoginPanelLesson/LoginPanelLesson/obj/Debug/net5.0-windows/App.g.i.cs
+++ LoginPanelLesson/LoginPanelLesson/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace LoginPanelLesson {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/LoginPanelLesson;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LoginPanelLesson;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
